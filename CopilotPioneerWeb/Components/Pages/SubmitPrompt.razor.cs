536cefb39b434970c20249874161a5b360557603
Jean-François Bilodeau <jfbilodeau@chronogears.com>
2024-05-07 17:01:42 -0400

    WIP

--- CopilotPioneerWeb/Components/Pages/SubmitPrompt.razor.cs
+++ CopilotPioneerWeb/Components/Pages/SubmitPrompt.razor.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
 
 namespace CopilotPioneerWeb.Components.Pages;
 
 public partial class SubmitPrompt : ComponentBase
 {
-    private SubmitPromptModel Model { get; set; } = new SubmitPromptModel();
+    [Inject]
+    private PioneerService PioneerService { get; set; } = default!;
     
+    [SupplyParameterFromForm]
+    private SubmitPromptModel Model { get; set;  } = new();
+
     private void OnSubmit()
     {
         throw new NotImplementedException();
@@ -16,6 +21,8 @@ sealed class SubmitPromptModel
 {
     public string Author { get; set; } = "";
     public string Product { get; set; } = "";
+    
+    [Required]
     public string Title { get; set; } = "";
     public string Prompt { get; set; } = "";
     public string Notes { get; set; } = "";
---
+++ CopilotPioneerWeb/PioneerService.cs
@@ -0,0 +1,24 @@
+﻿namespace CopilotPioneerWeb;
+
+public class Product
+{
+    public string Id { get; set; } = "";
+    public string Label { get; set; } = "";
+}
+
+public class PioneerService
+{
+    public Product[] GetProducts()
+    {
+        return
+        [
+            new Product { Id = "M365", Label = "Microsoft 365" },
+            new Product { Id = "Excel", Label = "Excel" },
+            new Product { Id = "Loop", Label = "Loop" },
+            new Product { Id = "PowerPoint", Label = "PowerPoint" },
+            new Product { Id = "Teams", Label = "Teams" },
+            new Product { Id = "Whiteboard", Label = "Whiteboard" },
+            new Product { Id = "Word", Label = "Word" }
+        ];
+    }
+}
