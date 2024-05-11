a70a3565e20d2368584c5ed731407f1ff8186dc7
Jean-François Bilodeau <jfbilodeau@chronogears.com>
2024-05-11 13:38:51 -0400

    Can save submissions

---
+++ CopilotPioneerWeb/CopilotPioneer.Web/Pages/Submissions.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using CopilotPioneer.Web.Models;
+using CopilotPioneer.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CopilotPioneer.Web.Pages;
+
+public class Submissions(PioneerService pioneerService) : PageModel
+{
+    [BindProperty(SupportsGet = true)]
+    public string? Id { get; set; } = null;
+    public Submission? Submission { get; set; } = null;
+
+    public async Task<IActionResult> OnGet()
+    {
+        if (Id != null)
+        {
+            Submission = await pioneerService.GetSubmissionById(Id);
+
+            if (Submission != null)
+            {
+                return Page();
+            }
+        }
+
+        return NotFound();
+    }
+}
