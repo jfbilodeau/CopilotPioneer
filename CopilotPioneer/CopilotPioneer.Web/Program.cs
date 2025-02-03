278364616c674bc299b13d8ce065b6660ad57a9f
J-F Bilodeau <jfbilodeau@chronogears.com>
2025-02-03 13:15:45 -0500

    UI updates for v2

--- CopilotPioneer/CopilotPioneer.Web/Program.cs
+++ CopilotPioneer/CopilotPioneer.Web/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddAuthorization(options =>
     // By default, all incoming requests will be authorized according to the default policy.
     options.FallbackPolicy = options.DefaultPolicy;
 });
+
 builder.Services.AddRazorPages(options =>
     {
         options.Conventions.AuthorizePage("/Admin", "Administrator");
